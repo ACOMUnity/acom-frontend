712661ae3cefa02d29ded210fa416c0c426d05e0
ADSactly <adsactly@gmail.com>
2024-04-03 17:19:43 -0400

    Update CountDownTwo.jsx

--- components/CountDown/CountDownTwo.jsx
+++ components/CountDown/CountDownTwo.jsx
@@ -24,7 +24,7 @@ const CountDownTwo = () => {
       return (
         <>
           <div className="time-count day">
-            <span>{days < 60 ? "0" + days : days}</span>Days
+            <span>{days < 10 ? "0" + days : days}</span>Days
           </div>
           <div className="time-count hour">
             <span>{hours < 10 ? "0" + hours : hours}</span>hour
